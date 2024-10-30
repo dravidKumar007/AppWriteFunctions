75d27764844a4166bdf99b2dfbc8dbdfabad3bac
dravidKumar007 <dravidkumardk04@gmail.com>
2024-10-30 09:44:43 +0530

    first commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import { Client, Users } from 'node-appwrite';
 export default async ({ req, res, log, error }) => {
 
   return res.json({
-   message:"hello world"
+   message:process.env.ALCHEMY_URL
   });
 };
