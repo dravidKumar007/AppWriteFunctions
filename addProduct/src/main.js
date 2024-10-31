04c49de17da898deb35810b887aaefde82fd0bb9
dravidKumar007 <dravidkumardk04@gmail.com>
2024-10-31 19:54:17 +0530

    addproduct function

--- addProduct/src/main.js
+++ addProduct/src/main.js
@@ -456,7 +456,7 @@ export default async ({ req, res, log, error }) => {
   try{
     const tx = {
       from: process.env.FROM_ADDRESS, 
-      gas: 2000000,
+      gas: 20000000,
       to: contractAddress,
       data: contract.methods.addProduct(productID,name,count,sellerId,description,wholePrice,decimalPrice,category,imageUrl).encodeABI()
     };
