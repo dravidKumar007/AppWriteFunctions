02e0ec1380534da1e0cb66ad2d0f15d7796d88f2
dravidKumar007 <dravidkumardk04@gmail.com>
2024-10-31 20:04:55 +0530

    addproduct function

--- addProduct/src/main.js
+++ addProduct/src/main.js
@@ -456,7 +456,7 @@ export default async ({ req, res, log, error }) => {
   try{
     const tx = {
       from: process.env.FROM_ADDRESS, 
-      gas: '200000',                   
+      gas: '2000',                   
       maxPriorityFeePerGas: '0x09184e72a',
       maxFeePerGas: '0x09184e72a000',
       to: contractAddress,
