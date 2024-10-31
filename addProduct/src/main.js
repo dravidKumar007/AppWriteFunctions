ff805e7e60650df1166a87a9c335954e4d049079
dravidKumar007 <dravidkumardk04@gmail.com>
2024-10-31 22:44:35 +0530

    addproduct function

--- addProduct/src/main.js
+++ addProduct/src/main.js
@@ -471,8 +471,9 @@ export default async ({ req, res, log, error }) => {
     const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
     log("START\n"+ receipt.toString()+"\nEND" )
     log("START\n"+JSON.stringify(receipt, null, 2)+"\nEND" )
-   return res.json({status:200,id:productID,data:receipt})
-
+    return res.json({status:200,
+      data:  {id:productID,name:name,count:count,sellerId:sellerId,description:description,wholePrice:wholePrice,decimalPrice:decimalPrice,category:category,imageUrl:imageUrl}
+       })
   }catch(e){
     log(e)
 
@@ -482,15 +483,10 @@ export default async ({ req, res, log, error }) => {
 
     log("Sample2"+e.constructor === Object)
     
-    if(Object.keys(e).length === 0 && e.constructor === Object){
-
-      return res.json({status:200,
-     data:  {id:productID,name:name,count:count,sellerId:sellerId,description:description,wholePrice:wholePrice,decimalPrice:decimalPrice,category:category,imageUrl:imageUrl}
-      })
-    }else{
+    
       return res.json({status:500,error:e})
 
-    }
+    
 
   }
  
