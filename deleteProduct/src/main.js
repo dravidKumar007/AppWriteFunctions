9d014ba882af71fa79584cb176715fb98ceda2ae
dravidKumar007 <dravidkumardk04@gmail.com>
2024-11-01 22:07:25 +0530

    new changes in delete

--- deleteProduct/src/main.js
+++ deleteProduct/src/main.js
@@ -451,10 +451,10 @@ export default async ({ req, res, log, error }) => {
   const contractAddress = process.env.CONTRACT_ADDRESS;
   const contract = new web3.eth.Contract(contractABI, contractAddress);
 
-  var {productID}=req.body.json()||{};
+  var productID=req.body.productID||{};
 
   log("My PRODUCTID is :"+productID+"\n")
-  log("My BODY  is :"+req.body.json()+"\n")
+  log("My BODY  is :"+req.body+"\n")
 
  
   try{
